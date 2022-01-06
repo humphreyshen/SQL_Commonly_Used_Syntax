5e040c3fa59b4434ef42676a3acc125495f3bc6c
humphreyshen <humphreyshen1003@gmail.com>
2022-01-06 18:33:25 +0800

    main

---
+++ taiwan_city_id_transfer.sql
@@ -0,0 +1,71 @@
+UPDATE `dDt_alumnidb_personal_251_1250` 
+SET `county`= 'A' WHERE county='台北市';
+
+UPDATE `dDt_alumnidb_personal_251_1250` 
+SET `county`= 'B' WHERE county='台中市';
+
+UPDATE `dDt_alumnidb_personal_251_1250` 
+SET `county`= 'B' WHERE county='臺中市';
+
+UPDATE `dDt_alumnidb_personal_251_1250`  
+SET `county`= 'C' WHERE county='基隆市';
+
+UPDATE `dDt_alumnidb_personal_251_1250` 
+SET `county`= 'D' WHERE county='台南市';
+
+UPDATE `dDt_alumnidb_personal_251_1250` 
+SET `county`= 'D' WHERE county='臺南市';
+
+UPDATE `dDt_alumnidb_personal_251_1250` 
+SET `county`= 'E' WHERE county='高雄市';
+
+UPDATE `dDt_alumnidb_personal_251_1250`  
+SET `county`= 'F' WHERE county='新北市';
+
+UPDATE `dDt_alumnidb_personal_251_1250`  
+SET `county`= 'G' WHERE county='宜蘭縣';
+
+UPDATE `dDt_alumnidb_personal_251_1250` 
+SET `county`= 'H' WHERE county='桃園市';
+
+UPDATE `dDt_alumnidb_personal_251_1250`  
+SET `county`= 'I' WHERE county='嘉義市';
+
+UPDATE `dDt_alumnidb_personal_251_1250` 
+SET `county`= 'J' WHERE county='新竹縣';
+
+UPDATE `dDt_alumnidb_personal_251_1250` 
+SET `county`= 'K' WHERE county='苗栗縣';
+
+UPDATE `dDt_alumnidb_personal_251_1250`  
+SET `county`= 'M' WHERE county='南投縣';
+
+UPDATE `dDt_alumnidb_personal_251_1250`  
+SET `county`= 'N' WHERE county='彰化縣';
+
+UPDATE `dDt_alumnidb_personal_251_1250`  
+SET `county`= 'O' WHERE county='新竹市';
+
+UPDATE `dDt_alumnidb_personal_251_1250`  
+SET `county`= 'P' WHERE county='雲林縣';
+
+UPDATE `dDt_alumnidb_personal_251_1250` 
+SET `county`= 'Q' WHERE county='嘉義縣';
+
+UPDATE `dDt_alumnidb_personal_251_1250`  
+SET `county`= 'T' WHERE county='屏東縣';
+
+UPDATE `dDt_alumnidb_personal_251_1250`  
+SET `county`= 'U' WHERE county='花蓮縣';
+
+UPDATE `dDt_alumnidb_personal_251_1250`  
+SET `county`= 'V' WHERE county='台東縣';
+
+UPDATE `dDt_alumnidb_personal_251_1250`
+SET `county`= 'W' WHERE county='金門縣';
+
+UPDATE `dDt_alumnidb_personal_251_1250`  
+SET `county`= 'X' WHERE county='澎湖縣';
+
+UPDATE `dDt_alumnidb_personal_251_1250`  
+SET `county`= 'Z' WHERE county='連江縣';
