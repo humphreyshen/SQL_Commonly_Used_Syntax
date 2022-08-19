7a36ae86e2a2478de6dec1b734c328e2efbf58cf
Humphrey Shen <92772809+humphreyshen@users.noreply.github.com>
2022-08-20 00:12:05 +0800

    modified:   taiwan_city_id_transfer.sql

--- taiwan_city_id_transfer.sql
+++ taiwan_city_id_transfer.sql
@@ -1,71 +1,71 @@
-UPDATE `dDt_alumnidb_personal_251_1250` 
+UPDATE `Desired_Column` 
 SET `county`= 'A' WHERE county='台北市';
 
-UPDATE `dDt_alumnidb_personal_251_1250` 
+UPDATE `Desired_Column` 
 SET `county`= 'B' WHERE county='台中市';
 
-UPDATE `dDt_alumnidb_personal_251_1250` 
+UPDATE `Desired_Column` 
 SET `county`= 'B' WHERE county='臺中市';
 
-UPDATE `dDt_alumnidb_personal_251_1250`  
+UPDATE `Desired_Column`  
 SET `county`= 'C' WHERE county='基隆市';
 
-UPDATE `dDt_alumnidb_personal_251_1250` 
+UPDATE `Desired_Column` 
 SET `county`= 'D' WHERE county='台南市';
 
-UPDATE `dDt_alumnidb_personal_251_1250` 
+UPDATE `Desired_Column` 
 SET `county`= 'D' WHERE county='臺南市';
 
-UPDATE `dDt_alumnidb_personal_251_1250` 
+UPDATE `Desired_Column` 
 SET `county`= 'E' WHERE county='高雄市';
 
-UPDATE `dDt_alumnidb_personal_251_1250`  
+UPDATE `Desired_Column`  
 SET `county`= 'F' WHERE county='新北市';
 
-UPDATE `dDt_alumnidb_personal_251_1250`  
+UPDATE `Desired_Column`  
 SET `county`= 'G' WHERE county='宜蘭縣';
 
-UPDATE `dDt_alumnidb_personal_251_1250` 
+UPDATE `Desired_Column` 
 SET `county`= 'H' WHERE county='桃園市';
 
-UPDATE `dDt_alumnidb_personal_251_1250`  
+UPDATE `Desired_Column`  
 SET `county`= 'I' WHERE county='嘉義市';
 
-UPDATE `dDt_alumnidb_personal_251_1250` 
+UPDATE `Desired_Column` 
 SET `county`= 'J' WHERE county='新竹縣';
 
-UPDATE `dDt_alumnidb_personal_251_1250` 
+UPDATE `Desired_Column` 
 SET `county`= 'K' WHERE county='苗栗縣';
 
-UPDATE `dDt_alumnidb_personal_251_1250`  
+UPDATE `Desired_Column`  
 SET `county`= 'M' WHERE county='南投縣';
 
-UPDATE `dDt_alumnidb_personal_251_1250`  
+UPDATE `Desired_Column`  
 SET `county`= 'N' WHERE county='彰化縣';
 
-UPDATE `dDt_alumnidb_personal_251_1250`  
+UPDATE `Desired_Column`  
 SET `county`= 'O' WHERE county='新竹市';
 
-UPDATE `dDt_alumnidb_personal_251_1250`  
+UPDATE `Desired_Column`  
 SET `county`= 'P' WHERE county='雲林縣';
 
-UPDATE `dDt_alumnidb_personal_251_1250` 
+UPDATE `Desired_Column` 
 SET `county`= 'Q' WHERE county='嘉義縣';
 
-UPDATE `dDt_alumnidb_personal_251_1250`  
+UPDATE `Desired_Column`  
 SET `county`= 'T' WHERE county='屏東縣';
 
-UPDATE `dDt_alumnidb_personal_251_1250`  
+UPDATE `Desired_Column`  
 SET `county`= 'U' WHERE county='花蓮縣';
 
-UPDATE `dDt_alumnidb_personal_251_1250`  
+UPDATE `Desired_Column`  
 SET `county`= 'V' WHERE county='台東縣';
 
-UPDATE `dDt_alumnidb_personal_251_1250`
+UPDATE `Desired_Column`
 SET `county`= 'W' WHERE county='金門縣';
 
-UPDATE `dDt_alumnidb_personal_251_1250`  
+UPDATE `Desired_Column`  
 SET `county`= 'X' WHERE county='澎湖縣';
 
-UPDATE `dDt_alumnidb_personal_251_1250`  
+UPDATE `Desired_Column`  
 SET `county`= 'Z' WHERE county='連江縣';
